e8f119cc7ebab093e77151931014f6a9af734b73
moupriyaroy <moupriya.roy@nagarro.com>
2023-02-22 14:52:03 +0530

    first commit

---
+++ src/main/java/com/example/azure/Employee.java
@@ -0,0 +1,27 @@
+package com.example.azure;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Employee {
+	@Id
+    @GeneratedValue
+	private int id;
+	private String name;
+	private String dept;
+	private long salary;
+
+}
